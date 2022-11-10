3c1971a926de24ee6613f4e2ef3a7eb1b674cb2c
Web <web@it.step>
2022-11-10 21:01:46 +0300

    start angular

---
+++ index.js
@@ -0,0 +1,13 @@
+// import cowsay from 'cowsay'
+
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "I'm a moooodule",
+    e : "oO",
+    T : "U "
+}));
+
+// import { say } from 'cowsay';
+
+// console.log(say({ text: 'grazing in the browser' }));
