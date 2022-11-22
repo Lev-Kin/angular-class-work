c03f1c36bbd283e6fd50d9b41e256bb0e849ef7a
Web <web@it.step>
2022-11-22 17:57:29 +0300

    start topic pipes

--- app-new/src/app/app.component.ts
+++ app-new/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app-new';
+  title = 'Pipes and Directives';
   text = 'To be or not to be...';
   name = 'adfadf';
 
--- app-new/src/app/app.module.ts
+++ app-new/src/app/app.module.ts
@@ -5,16 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 // для работы с формами и привязки данных
 import { FormsModule } from '@angular/forms';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { NavComponent } from './nav/nav.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    NavComponent
   ],
   imports: [
     BrowserModule,
