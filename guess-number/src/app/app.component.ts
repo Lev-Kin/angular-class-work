a4a31e786dfa9f5a4f38923cecbca3e395352e1e
Web <web@it.step>
2022-11-24 20:20:35 +0300

    add theory child companent

--- guess-number/src/app/app.component.ts
+++ guess-number/src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'guess-number';
+  hiddenNumber = Math.round(Math.random() * 100); // задуманное число
+  guess = 0;
+  deviation = -101;
+  count = 0;
+
+  check(): void {
+    this.deviation = this.guess - this.hiddenNumber;
+    this.count++;
+  }
+
+  reset(): void {
+    this.hiddenNumber = Math.round(Math.random() * 100); // задуманное число
+    this.guess = 0;
+    this.deviation = -101;
+    this.count = 0;
+  }
 }
--- guess-number/src/app/app.module.ts
+++ guess-number/src/app/app.module.ts
@@ -4,15 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+import { ChildComponent } from './child/child.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ChildComponent],
+  imports: [FormsModule, BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
